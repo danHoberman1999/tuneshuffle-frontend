39bfa1153740711c5cbf72086419c14d074d4ba0
Daniel Hoberman <dhoberman@zagmail.gonzaga.edu>
2022-07-19 00:39:18 -0400

    updated background color changing style

--- src/Components/Shuffle/Shuffle.tsx
+++ src/Components/Shuffle/Shuffle.tsx
@@ -15,13 +15,23 @@ interface RandomDataProps {
   year?: string
 }
 
+interface ImageDataProps {
+  r?: number
+  g?: number
+  b?: number
+  hex?: string
+  hsv?: []
+}
+
 const Shuffle = () => {
   const [randomData, setRandomData] = useState<RandomDataProps>({})
   const [imageScanLoading, setImageScanLoading] = useState(true)
   const [imageAPILoading, setImageAPILoading] = useState(true)
   let [loaderColor, setLoaderColor] = useState('#ffffff')
   const [trackPlayStorage, setTrackPlayStorage] = useState('')
-  const [imageGradient, setImageGradient] = useState([])
+  const [imageGradientOther, setImageGradientOther] = useState([])
+  const [imageGradientDominant, setImageGradientDominant] =
+    useState<ImageDataProps>({})
   const [firstLoad, setFirstLoad] = useState(true)
   const [currentGenre, setCurrentGenre] = useState<string | undefined>('')
   const [mode, setMode] = useState<'light' | 'dark' | undefined>(
@@ -80,7 +90,9 @@ const Shuffle = () => {
           api_secret: CONSTANTS.SIGHT_ENGINE_SECRET,
         },
       })
-      setImageGradient(response['data']['colors']['other'])
+      setImageGradientOther(response['data']['colors']['other'])
+      setImageGradientDominant(response['data']['colors']['dominant'])
+      console.log(response['data']['colors']['accent'])
       setImageScanLoading(false)
       if (response['data']['colors']['other'].length < 2) {
         setImageScanLoading(true)
@@ -110,7 +122,6 @@ const Shuffle = () => {
   }, [])
 
   useEffect(() => {
-    console.log(randomData)
     if (window.location.hash) {
       const { access_token, expires_in, token_type } =
         GLOBAL_METHODS.getReturnedParamsFromSpotifyAuth(window.location.hash)
@@ -144,14 +155,13 @@ const Shuffle = () => {
       className='container'
       style={{
         background: imageScanLoading
-          ? 'linear-gradient(#ec008c, #fc6767)'
-          : 'linear-gradient(' +
-            `${imageGradient[0]['hex']}` +
-            ',' +
-            `${imageGradient[1]['hex']}` +
-            ')',
-        animation: 'fadein 1s',
-        animationDelay: '2.5s',
+          ? 'linear-gradient(to top left, #4ca1af, transparent),linear-gradient(to top right, #2c3e50, transparent)'
+          : 'linear-gradient(to top left, ' +
+            `${imageGradientDominant['hex']}` +
+            ', transparent),linear-gradient(to top right, ' +
+            `${imageGradientOther[0]['hex']}` +
+            ', transparent)',
+        backgroundBlendMode: 'screen',
       }}
     >
       <div className='shuffleContainer'>
@@ -176,7 +186,6 @@ const Shuffle = () => {
           className='music-info-container'
           style={{
             background: mode === 'dark' ? '#1c1c1e' : '#d6d6c1',
-            opacity: '0.7',
           }}
         >
           <h1
